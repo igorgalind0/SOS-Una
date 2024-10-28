9f07d1c175ebd13256e2febd9a5506bb90ab740a
Igor Galindo <igorgalindo950@gmail.com>
2024-10-28 18:06:01 -0300

    footer

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
         button.classList.toggle('active');
         document.body.classList.toggle('dark-mode');
+        document.body.style.transition = '0.2s';
 
         if (document.body.classList.contains('dark-mode')) {
             localStorage.setItem('dark-mode', 'enabled');
